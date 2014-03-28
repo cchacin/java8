1b1dffd593c8efcc5395fcd423d07567823956c0
Carlos Chacin <cchacin@groupon.com>
2014-03-27 23:26:24 -0300

    Behavior

--- Behavior.java
+++ Behavior.java
@@ -4,6 +4,16 @@ public class Behavior {
 
     private static final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
+    public static int sumAll(List<Integer> numbers) {
+        int total = 0;
+
+        for (Integer n : numbers) {
+            total += n;
+        }
+
+        return total;
+    }
+
     public static void main(final String args[]) {
 
     }
