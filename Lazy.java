d59a88b097f251bbc8dc0a9f154bdb9ee61b6a82
Carlos Chacin <cchacin@groupon.com>
2014-03-28 00:04:23 -0300

    Lazy imperative decomposition

--- Lazy.java
+++ Lazy.java
@@ -4,6 +4,21 @@ public class Lazy {
 
     private static final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
+    public static boolean isEven(int n) {
+        System.out.println(n);
+        return n % 2 == 1;
+    }
+
+    public static int doubleIt(int n) {
+        System.out.println(n);
+        return n * 2;
+    }
+
+    public static boolean isGT5(int n) {
+        System.out.println(n);
+        return n > 5;
+    }
+
     public static void main(final String args[]) {
 
         // Take only a even numbers, double them and print
@@ -18,5 +33,22 @@ public class Lazy {
                 }
             }
         }
+
+        List<Integer> l1 = new ArrayList<>();
+        for (int n : numbers) {
+            if(isEven(n)) l1.add(n);
+        }
+
+        List<Integer> l2 = new ArrayList<>();
+        for (int n : l1) {
+            l2.add(doubleIt(n));
+        }
+
+        List<Integer> l3 = new ArrayList<>();
+        for (int n : l2) {
+            if(isGT5(n)) l3.add(n);
+        }
+
+        System.out.println(l3.get(0));
     }
 }
