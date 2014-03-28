11d2d448a558efcbcf249ba0df3be29c01edf649
Carlos Chacin <cchacin@groupon.com>
2014-03-28 01:16:40 -0300

    Traditional non OO way

--- Parallelism.java
+++ Parallelism.java
@@ -5,6 +5,12 @@ public class Parallelism {
 
     public static void main(final String[] args) {
 
+        File current = new File(".");
 
+        File[] children = current.listFiles();
+
+        for(File child : children) {
+            System.out.println(child);
+        }
     }
 }
